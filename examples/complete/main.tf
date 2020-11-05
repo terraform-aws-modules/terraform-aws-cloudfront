c4b894033a605dfd16d9f899ddc6f5ad7e720a69
David Wolffberg <1350533+Wolffberg@users.noreply.github.com>
2020-11-05 09:29:24 +0100

    change path in s3 bucket policy

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -252,7 +252,7 @@ module "records" {
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions   = ["s3:GetObject"]
-    resources = ["${module.s3_one.this_s3_bucket_arn}/uploads/*"]
+    resources = ["${module.s3_one.this_s3_bucket_arn}/static/*"]
 
     principals {
       type        = "AWS"
