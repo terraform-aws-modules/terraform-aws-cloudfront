b6c840e775f9701f05b96191905358426f3d4484
Anton Babenko <anton@antonbabenko.com>
2021-03-22 10:15:03 +0100

    fix: Fixed type of ordered_cache_behavior

--- variables.tf
+++ variables.tf
@@ -129,6 +129,6 @@ variable "default_cache_behavior" {
 
 variable "ordered_cache_behavior" {
   description = "An ordered list of cache behaviors resource for this distribution. List from top to bottom in order of precedence. The topmost cache behavior will have precedence 0."
-  type        = list(any)
+  type        = any
   default     = []
 }
