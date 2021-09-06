403a11a7b9ba0aae4ef4cfb63398f5db5bbc5648
Théophile Helleboid - chtitux <chtitux@gmail.com>
2021-09-06 13:55:10 +0400

    docs: Add TLSv1.1 and TLSv1.2 for origin_ssl_protocols in examples (#51)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -45,7 +45,7 @@ module "cloudfront" {
         http_port              = 80
         https_port             = 443
         origin_protocol_policy = "match-viewer"
-        origin_ssl_protocols   = ["TLSv1"]
+        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
       }
 
       custom_header = [
