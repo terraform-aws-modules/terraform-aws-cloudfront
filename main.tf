d31306d96caf66ae88f651425921175132f0322f
Gabriel Martinez <19713226+GMartinez-Sisti@users.noreply.github.com>
2022-01-14 11:06:19 +0000

    fix: Add lifecycle clause for aws_cloudfront_origin_access_identity (#65)

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ resource "aws_cloudfront_origin_access_identity" "this" {
   for_each = local.create_origin_access_identity ? var.origin_access_identities : {}
 
   comment = each.value
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_cloudfront_distribution" "this" {
