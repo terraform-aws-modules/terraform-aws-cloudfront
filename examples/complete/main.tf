2fd91de9b4da2b4db8fb0bfeb79e5f182edbc60a
Rémi FLAMENT <remi.flament.78@gmail.com>
2023-01-30 17:55:57 +0100

    add OAC IAM policy in the complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -309,6 +309,30 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = module.s3_one.s3_bucket_id
   policy = data.aws_iam_policy_document.s3_policy.json
 }
+  
+########################
+# Origin Access Controls
+########################
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${module.s3_one.s3_bucket_arn}/static/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [module.cloudfront.cloudfront_distribution_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = module.s3_one.s3_bucket_id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
 
 ########
 # Extra
